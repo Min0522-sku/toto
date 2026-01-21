bd31f6559cb01d1865abd73c488a43c7017dc455
jisung <jisung@example.com>
2026-01-21 17:23:47 +0900

    feat: 공통 함수 추가(예상승률계산 함수, id로 배열 속 객체 찾는 함수)

--- js/common/common.js
+++ js/common/common.js
@@ -1,4 +1,4 @@
-// [1] header와 footer를 각 div에 print
+// header와 footer를 각 div에 print
 
 async function loadHTML(selector, url) {
     const container = document.querySelector(selector);
@@ -14,4 +14,29 @@ document.addEventListener("DOMContentLoaded", () => {
     loadHTML("#footer", "/html/common/footer.html");
 });
 
-// [2] 사용자의 이름과 자본금을 헤더에 print
+// 사용자의 이름과 자본금을 헤더에 print
+
+// 예상 승률 계산 함수 (홈 공격력, 홈 수비력, 원정 공격력, 원정 수비력)
+function getWinrate(home_offense, home_defense, away_offense, away_defense){
+    let diff = (home_offense - away_defense) + (home_defense - away_offense);
+    let home_adventage = 0.05; // 홈 어드벤티지 값 (수정 가능)
+    let responsiveness = 0.005; // 민감도 (수정 가능) (diff가 20이면 +0.1 ; 이 값이 커지면 공격력/수비력 차에 따른 승률이 급격히 차이남)
+
+    let home_win_prob = 0.5 + diff * responsiveness + home_adventage; 
+
+    // 승률을 5% ~ 95% 사이로 고정
+    if(home_win_prob < 0.05) home_win_prob = 0.05;
+    else if(home_win_prob > 0.95) home_win_prob = 0.95;
+
+    return home_win_prob;
+}
+
+// id로 배열 속 객체 찾는 함수 (array : 찾을 객체가 속한 배열, id : 그 객체에서 찾을 아이디)
+function getObjById(array, id){
+    for(let i = 0; i < array.length; i++){
+        if(array[i].id == id){
+            return array[i];
+        }
+    }
+    return null;
+}
