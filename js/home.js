1415ca2a1e8c50f45ffce29842b713c6b5da6493
jisung <jisung@example.com>
2026-01-23 12:05:16 +0900

    style: home화면 전체적인 ui 개선

--- js/home.js
+++ js/home.js
@@ -25,7 +25,7 @@ function printTodayMatch() {
     
     // [3] id=todayTable div (오늘의 경기) 에 html 주입
     let todayTable = document.querySelector("#todayTable");
-    let html = `<ul class="table-row">
+    let html = `<ul class="table-row table-header">
                     <li class="table-item">스포츠명</li>
                     <li class="table-item">리그</li>
                     <li class="table-item">홈팀</li>
@@ -58,8 +58,8 @@ function printTodayMatch() {
 
         // [3-2] html에 주입 후 innerHTML
 
-        if(i == 0) html += `<ul class="table-row" style="margin-top: 24px;">`; // 첫 번째 매치아이템은 헤더로부터 24px margin
-        else html += `<ul class="table-row">`; // 나머지 매치아이템은 위 아이템과 붙여서
+        if(i == 0) html += `<ul class="table-row table-body-row" style="margin-top: 24px;">`; // 첫 번째 매치아이템은 헤더로부터 24px margin
+        else html += `<ul class="table-row table-body-row">`; // 나머지 매치아이템은 위 아이템과 붙여서
 
         html += `<li class="table-item">
                     <img class="sports-img center" src="${sportImgUrl}">
@@ -99,7 +99,7 @@ function printPastMatch() {
     
     // [3] id=todayTable div (오늘의 경기) 에 html 주입
     let pastTable = document.querySelector("#pastTable");
-    let html = `<ul class="table-row">
+    let html = `<ul class="table-row table-header">
                     <li class="table-item">스포츠명</li>
                     <li class="table-item">리그</li>
                     <li class="table-item">홈팀</li>
@@ -133,8 +133,8 @@ function printPastMatch() {
 
         // [3-2] html에 주입 후 innerHTML
 
-        if(i == 0) html += `<ul class="table-row" style="margin-top: 24px;">`; // 첫 번째 매치아이템은 헤더로부터 24px margin
-        else html += `<ul class="table-row">`; // 나머지 매치아이템은 위 아이템과 붙여서
+        if(i == 0) html += `<ul class="table-row table-body-row" style="margin-top: 24px;">`; // 첫 번째 매치아이템은 헤더로부터 24px margin
+        else html += `<ul class="table-row table-body-row">`; // 나머지 매치아이템은 위 아이템과 붙여서
 
         html += `<li class="table-item">
                     <img class="sports-img center" src="${sportImgUrl}">
